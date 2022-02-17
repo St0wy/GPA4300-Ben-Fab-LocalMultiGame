3b3b8c44f491285d61f644a0ea1e6362576379ca
Fabian Huber <stowy@protonmail.ch>
2022-02-17 16:15:13 +0100

    made light follow a player position

---
+++ Assets/Scripts/PlayerFollowerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace LocalMultiplayerGame
+{
+	public class PlayerFollowerBehaviour : MonoBehaviour
+	{
+		[SerializeField] private Transform playerPos;
+
+		private void Update()
+		{
+			transform.localPosition = playerPos.localPosition;
+		}
+	}
+}
