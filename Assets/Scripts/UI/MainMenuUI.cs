ebd4ce266f68183c137a3325444b57759419afb8
Fabian Huber <stowy@protonmail.ch>
2022-02-22 15:24:21 +0100

    hid cursor

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace LocalMultiplayerGame.UI
 {
     public class MainMenuUI : MonoBehaviour
     {
+        private void Start()
+        {
+            Cursor.visible = false;
+        }
+
         public void LoadGame()
         {
             SceneManager.LoadScene(1);
