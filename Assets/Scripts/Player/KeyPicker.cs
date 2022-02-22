b27c81e4270e17c5384675f29da6f743fbecd8bb
Fabian Huber <stowy@protonmail.ch>
2022-02-22 14:58:12 +0100

    connected level 2

---
+++ Assets/Scripts/Player/KeyPicker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LocalMultiplayerGame.Player
+{
+	public class KeyPicker : MonoBehaviour
+	{
+		public const string KeyTag = "Key";
+		
+		[SerializeField] private Tile tileToSet;
+		[SerializeField] private Tilemap tilemap;
+		[SerializeField] private Vector3Int[] tilesToChange;
+
+		public bool HasKey { get; private set; }
+
+		private void OnTriggerEnter2D(Collider2D col)
+		{
+			// Check if we collided with a key
+			if (!col.gameObject.CompareTag(KeyTag))
+				return;
+
+			Destroy(col.gameObject);
+			HasKey = true;
+
+			foreach (Vector3Int tilePos in tilesToChange)
+			{
+				tilemap.SetTile(tilePos, tileToSet);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI/WinMenuUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LocalMultiplayerGame.UI
+{
+    public class WinMenuUI : MonoBehaviour
+    {
+        public void LoadMainMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
